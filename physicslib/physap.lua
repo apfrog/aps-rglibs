1dc0e9a8a478758535fc233d00a468108b7fffc4
IncognitoMan <thecatwithawp@gmail.com>
2022-12-10 12:43:13 +0100

    Added a README for the individual libraries

--- physicslib/physap.lua
+++ physicslib/physap.lua
@@ -14,15 +14,15 @@ function(startpoint:vec2, endpoint:vec2, vc:VideoChip, cubeheight:number)
 end
 
 physap.gravity = -- Returns the updated startpoint, endpoint and acceleration 
-function(startpos:vec2, endpoint:vec2, acceleration:number, fallspeed:number, cpu:CPU, grounded:boolean)
+function(startpoint:vec2, endpoint:vec2, acceleration:number, fallspeed:number, cpu:CPU, grounded:boolean)
 	if not grounded then 
 		acceleration += cpu.DeltaTime
 	else
 		acceleration = 0
 	end
-	startpos += vec2(0, (fallspeed * 9.8 * acceleration))
+	startpoint += vec2(0, (fallspeed * 9.8 * acceleration))
 	endpoint += vec2(0, (fallspeed * 9.8 * acceleration))
-	return startpos, endpoint, acceleration -- Assign in the cpu by doing: value, value1, value2 = physap.gravity(values here)
+	return startpoint, endpoint, acceleration -- Assign in the cpu by doing: value, value1, value2 = physap.gravity(values here)
 end
 
 return physap
